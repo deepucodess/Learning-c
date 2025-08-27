b6407501ad00e40fd04a3cc8d3aaf8bcc9ca4e25
deepucodess <onlyforstudy77477@gmail,com>
2025-08-27 23:16:56 +0530

    dumb college question part 1

---
+++ 5th.c
@@ -0,0 +1,12 @@
+//FIND AREA OF CIRCLE R=10(SORRY THIS IS COLLEGE QUESTION I KNOW THESE ARE DUMBS)
+
+#include <stdio.h>
+int main(){
+	float pie,area;
+	int r;
+	pie=3.14;
+	r=10;
+	area =pie*r*r;
+	printf("AREA OF CIRCLE = %f",area);
+return 0;	
+}
---
+++ 6th.c
@@ -0,0 +1,11 @@
+//This is for simple interest again i know this is dumb`
+#include <stdio.h>
+int main(){
+	int p,r,t,simple_interest;
+	p=1000;
+	r=5;
+	t=2;
+	simple_interest = (p*r*t)/100;
+	printf("simple interest= $%d",simple_interest);
+return 0;	
+}
