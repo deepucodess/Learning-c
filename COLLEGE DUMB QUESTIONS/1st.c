21f821b532cf79b119bdb155458c7977a709b1d2
deepucodess <onlyforstudy77477@gmail,com>
2025-08-27 23:24:26 +0530

    Updating folders in correct format once more

---
+++ COLLEGE DUMB QUESTIONS/1st.c
@@ -0,0 +1,10 @@
+//find area of reactangle l=10,b=20 (SORRY THIS IS COLLEGE QUESTION I KNOW THESE ARE DUMBS)
+#include <stdio.h>
+int main(){
+	int l,b,area;
+	l=10;
+	b=20;
+	area = l*b;
+	printf("AREA OF RECTANGLE = %d",area);
+return 0;	
+}
--- COLLEGE DUMB QUESTIONS/4th.c
+++ COLLEGE DUMB QUESTIONS/4th.c
@@ -1,10 +1,20 @@
-//find area of reactangle l=10,b=20 (SORRY THIS IS COLLEGE QUESTION I KNOW THESE ARE DUMBS)
+//This is simple calculator oh my god again i know this is super dumb
 #include <stdio.h>
 int main(){
-	int l,b,area;
-	l=10;
-	b=20;
-	area = l*b;
-	printf("AREA OF RECTANGLE = %d",area);
+	int a,b;
+	int add,subtract,divide,multiply,remainder;
+	a = 200;
+	b = 100;
+	add = a+b;
+	subtract = a-b;
+	divide = a/b;
+	multiply = a*b;
+	remainder = a%b;
+	printf("Sum of a and b is = %d\n",add);
+    printf("Subtract of a and b is = %d\n",subtract);
+    printf("Division of a and b is = %d\n",divide);
+    printf("Mulitplication of a and b is = %d\n",multiply);
+    printf("Remainder of a and b is = %d\n",remainder);
+
 return 0;	
 }
